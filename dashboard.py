a6f9cf7c997b1b926694309e0ae8a7de1dfd3a58
DimasPrsety <dzipras1@gmail.com>
2024-10-17 13:02:04 +0700

    Dirubah ke plotly

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,7 @@
 import streamlit as st
-from bokeh.plotting import figure
-from bokeh.io import output_file, show
-from bokeh.layouts import layout
-from bokeh.models import Div
-import sys
+import plotly.graph_objects as go
 
+# Data
 total_izin = 597
 average_izin = 712.1
 selesai_diproses = 433
@@ -16,49 +13,26 @@ selesai_perc = selesai_diproses / total_izin * 100
 ditolak_perc = ditolak_dibatalkan / total_izin * 100
 masih_perc = masih_diproses / total_izin * 100
 
-total_izin_box = Div(text = f"""
-    <div style = 'background-color: #f0f0f0; text-align:center; width: 200px; padding:10px;'>
-        <p> Total Izin yang diajukan </p>
-        <h2>{total_izin}</h2>
-    </div>
-""")
+# Set page config
+st.set_page_config(page_title = "DPMPTSP Dashboard", layout="wide")
 
-average_izin_box = Div(text=f"""
-    <div style = 'background-color: #f0f0f0; text-align:center; width: 200px; padding:10px;'>
-        <p> in average</p>
-        <p> total izin yang diajukan per kecamatan </p>
-        <h4>{average_izin}</h4>
-    </div>
-""")
+# Define layout using column in streamlit
+c1, c2 = st.columns(2)
+c1.metric(label = "Total Izin yang diajukan", value = total_izin)
+c2.metric(label ="Average izin per kecamatan", value = average_izin)
 
-selesai_izin_box = Div(text=f"""
-   <div style = 'background-color: #f0f0f0; text-align:center; width:200px; padding:10px;'>
-        <p> Selesai diproses </p>
-        <h2>{selesai_diproses}</h2> 
-        <p> {round(selesai_perc, 1)}%</p>
-    </div>
-""")
+c3, c4, c5 = st.columns(3)
+c3.metric(label = "Selesai diproses", value = selesai_diproses, delta=f"{round(selesai_perc, 1)}%")
+c4.metric(label = "Masih diproses", value = masih_diproses, delta=f"{round(masih_perc, 1)}%")
+c5.metric(label = "Ditolak & dibatalkan", value = ditolak_dibatalkan, delta=f"{round(ditolak_perc, 1)}%")
 
-masih_diproses_box = Div(text=f"""
-    <div style = 'background-color: #f0f0f0; text-align:center; width:200px; padding:10px;'>
-        <p> Masih diproses</p>
-        <h2>{masih_diproses}</h2>
-        <p>{round(masih_perc, 1)}%</p>
-    </div>
-""")
 
-ditolak_izin_box = Div(text=f"""
-    <div style = 'background-color: #f0f0f0; text-align:center; width:200px; padding:10px;'>
-        <p> Ditolak & Dibatalkan</p>
-        <h2>{ditolak_dibatalkan}</h2>
-        <p>{round(ditolak_perc, 1)}%</p>
-    </div>
-""")
+fig = go.Figure(go.Bar(
+    x = ['Selesai','Masih Diproses', 'Ditolak & Dibatalkan']
+    , y = [selesai_diproses, masih_diproses, ditolak_dibatalkan]
+    , marker_color = ['green', 'orange', 'red']
+))
 
-layout_dashboard = layout([
-    [total_izin_box, average_izin_box]
-    , [selesai_izin_box, masih_diproses_box, ditolak_izin_box]
-])
+fig.update_layout(title_text = "Distribution of Process Status", title_x = 0.5)
 
-print(sys.path)
-st.bokeh_chart(layout_dashboard)
+st.plotly_chart(fig, use_container_width = True)
