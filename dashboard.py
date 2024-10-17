173d08b34449923e8b36d2c4d5482a10695b18e7
DimasPrsety <dzipras1@gmail.com>
2024-10-17 09:39:53 +0700

    coba2 dulu

--- dashboard.py
+++ dashboard.py
@@ -18,40 +18,40 @@ masih_perc = masih_diproses / total_izin * 100
 
 total_izin_box = Div(text = f"""
     <div style = 'background-color: #f0f0f0; text-align:center; width: 200px; padding:10px;'>
-        <h2>{total_izin}</h2>
         <p> Total Izin yang diajukan </p>
+        <h2>{total_izin}</h2>
     </div>
 """)
 
 average_izin_box = Div(text=f"""
     <div style = 'background-color: #f0f0f0; text-align:center; width: 200px; padding:10px;'>
         <p> in average</p>
-        <h4>{average_izin}</h4>
         <p> total izin yang diajukan per kecamatan </p>
+        <h4>{average_izin}</h4>
     </div>
 """)
 
 selesai_izin_box = Div(text=f"""
    <div style = 'background-color: #f0f0f0; text-align:center; width:200px; padding:10px;'>
+        <p> Selesai diproses </p>
         <h2>{selesai_diproses}</h2> 
         <p> {round(selesai_perc, 1)}%</p>
-        <p> Selesai diproses </p>
     </div>
 """)
 
 masih_diproses_box = Div(text=f"""
     <div style = 'background-color: #f0f0f0; text-align:center; width:200px; padding:10px;'>
+        <p> Masih diproses</p>
         <h2>{masih_diproses}</h2>
         <p>{round(masih_perc, 1)}%</p>
-        <o> Masih diproses</p>
     </div>
 """)
 
 ditolak_izin_box = Div(text=f"""
     <div style = 'background-color: #f0f0f0; text-align:center; width:200px; padding:10px;'>
+        <p> Ditolak & Dibatalkan</p>
         <h2>{ditolak_dibatalkan}</h2>
         <p>{round(ditolak_perc, 1)}%</p>
-        <o> Ditolak & Dibatalkan</p>
     </div>
 """)
 
