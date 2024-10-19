5a62e4e8c61131b3aeed5ae5b8761738052bdde0
DimasPrsety <dzipras1@gmail.com>
2024-10-19 09:45:54 +0700

    testing tipis2

--- dashboard.py
+++ dashboard.py
@@ -75,7 +75,7 @@ with st.spinner('Updating Report .... ') :
     pcdf = pd.read_excel('ct_izin.xlsx', sheet_name = 'tot_bidang2')
     pcdf = pcdf[pcdf['service_point']==sp]
 
-    fig1 = go.Figure(data = [go.Pie(pcdf, labels ='bidang_recode', value = 'total_diajukan', hole = .3)])
+    fig1 = go.Figure(data = [go.Pie(labels =pcdf['bidang_recode'], value = pcdf['total_diajukan'], hole = .3)])
     fig1.update_traces(marker_color = '#264653')
     fig1.update_layout(title_text = "Kategori Bidang yang Dominan", title_x = 0, margin = dict(l=0, r=10, b=10))
 
